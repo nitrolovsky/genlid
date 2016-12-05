1425844147a75b4d79f0db2edfff9dbd6e3f2617
Pavel L <nitrolovsky@gmail.com>
2016-12-05 17:51:21 +0300

    Deleted image for vk.

--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -8,8 +8,6 @@
         <link rel="stylesheet" href="/vendor/bootstrap/dist/css/bootstrap.css">
         <link rel="stylesheet" href="/css/docs.min.css">
         <link rel="stylesheet" href="/css/custom.css">
-        <link rel="image_src" href="http://nitrolovsky.com/images/screenshot-nitrolovsky.com-2016-12-05-17-41-45.png">
-        <meta property="og:image" content="/images/screenshot-nitrolovsky.com-2016-12-05-17-41-45.png">
     </head>
     <body>
         @include('layouts.navbar')
