5d7c295973662c2234a9920a53f5360f5bd41364
nitrolovsky <nitrolovsky@gmail.com>
2017-02-19 12:13:04 +0300

    Added circle

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -10,15 +10,15 @@
                     <input type="hidden" name="product_url" value="{{ $source }}">
                     <input type="hidden" name="type" value="Order from article">
                     <div class="form-group {{ !empty($errors->orders->first('email')) ? 'has-danger' : ''}} {{ !empty(old('email')) ? 'has-success' : ''}}">
-                        <input type="email" class="form-control {{ !empty($errors->orders->first('email')) ? 'form-control-danger' : ''}} {{ !empty(old('email')) ? 'form-control-success' : ''}}" id="email" placeholder="Email" name="email" value="{{ old('email') }}">
+                        <input type="email" class="form-control btn-circle {{ !empty($errors->orders->first('email')) ? 'form-control-danger' : ''}} {{ !empty(old('email')) ? 'form-control-success' : ''}}" id="email" placeholder="Email" name="email" value="{{ old('email') }}">
                     </div>
                     <div class="form-group {{ !empty($errors->orders->first('phone')) ? 'has-danger' : ''}} {{ !empty(old('phone')) ? 'has-success' : ''}}">
-                        <input type="phone" class="form-control {{ !empty($errors->orders->first('phone')) ? 'form-control-danger' : ''}} {{ !empty(old('phone')) ? 'form-control-success' : ''}}" id="phone" placeholder="Телефон" name="phone" value="{{ old('phone') }}">
+                        <input type="phone" class="form-control btn-circle {{ !empty($errors->orders->first('phone')) ? 'form-control-danger' : ''}} {{ !empty(old('phone')) ? 'form-control-success' : ''}}" id="phone" placeholder="Телефон" name="phone" value="{{ old('phone') }}">
                     </div>
                     <div class="form-group">
-                        <textarea type="text" class="form-control" id="comment" placeholder="Комментарий (по желанию)" name="comment" value="{{ old('comment') }}" rows="3"></textarea>
+                        <textarea type="text" class="form-control btn-circle" id="comment" placeholder="Комментарий (по желанию)" name="comment" value="{{ old('comment') }}" rows="1"></textarea>
                     </div>
-                    <button type="submit" class="btn btn-primary btn-block" onclick="yaCounter42033909.reachGoal('OrderByPhone'); return true;"><span class="fa fa-fw fa-paper-plane" aria-hidden="true"></span>&nbsp;Отправить</button>
+                    <button type="submit" class="btn btn-primary btn-block btn-circle" onclick="yaCounter42033909.reachGoal('OrderByPhone'); return true;"><span class="fa fa-fw fa-paper-plane" aria-hidden="true"></span>&nbsp;Отправить</button>
                 </form>
                 <div class="small pt-3">
                     {{ $explanations }}
