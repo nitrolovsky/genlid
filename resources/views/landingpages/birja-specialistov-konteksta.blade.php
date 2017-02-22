80afb08091d696069b03da82f265e9c86e710ac8
Pavel L <nitrolovsky@gmail.com>
2017-02-22 16:51:57 +0300

    Added birja

---
+++ resources/views/landingpages/birja-specialistov-konteksta.blade.php
@@ -0,0 +1,186 @@
+@component("components.head")
+    @slot("title")
+        Биржа специалистов по контекстной рекламе Google Adwords и Яндекс Директ
+    @endslot
+@endcomponent
+
+<link rel="stylesheet" href="/css/imperium-stroy.css">
+
+@component("components.firstbg-2")
+    @slot("bgimageid")
+        bgimage-first
+    @endslot
+    @slot("image")
+        /images/birja-specialistov-konteksta/birja-specialistov-konteksta-1.jpg
+    @endslot
+    @slot("domen")
+        Genlid.com
+    @endslot
+    @slot("descriptor")
+        Биржа специалистов по контекстной рекламе Google Adwords и Яндекс Директ
+    @endslot
+    @slot("phone")
+        8 929 116 85 65
+    @endslot
+    @slot("heading")
+        Найдите себе специалиста по настройке контекстной рекламы в Google Adwords и Яндекс Директ
+    @endslot
+    @slot("calltoaction")
+        Оставить заказ
+    @endslot
+@endcomponent
+
+<div class="container pt-5 pb-5">
+    <div class="row">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-1.jpg" class="img-fluid mx-auto d-block" alt="Фрилансер">
+        </div>
+        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+            <h2 class="russo">
+                Павел Нитроловский
+            </h2>
+            <div class="pt-3">
+                📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение. 📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение. 📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение.
+            </div>
+            <br>
+            <a href="#request" class="btn btn-primary  btn-circle">Связаться со специалистом</a>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-1.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-2.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-3.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container pt-5 pb-5">
+    <div class="row">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-1.jpg" class="img-fluid mx-auto d-block" alt="Фрилансер">
+        </div>
+        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+            <h2 class="russo">
+                Павел Нитроловский
+            </h2>
+            <div class="pt-3">
+                📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение. 📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение. 📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение.
+            </div>
+            <br>
+            <a href="#request" class="btn btn-primary  btn-circle">Связаться со специалистом</a>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-1.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-2.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-3.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container pt-5 pb-5">
+    <div class="row">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-1.jpg" class="img-fluid mx-auto d-block" alt="Фрилансер">
+        </div>
+        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+            <h2 class="russo">
+                Павел Нитроловский
+            </h2>
+            <div class="pt-3">
+                📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение. 📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение. 📈 Настраиваю контекстную рекламу Google Adwords и Яндекс Директ. Лендинг в подарок 🎁. 20 000 р. за настройку и 10 % от бюджета за ведение.
+            </div>
+            <br>
+            <a href="#request" class="btn btn-primary  btn-circle">Связаться со специалистом</a>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-1.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-2.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+            <img src="/images/birja-specialistov-konteksta/birja-specialistov-konteksta-freelancer-primer-3.png" class="img-fluid mx-auto d-block" alt="Фрилансер">
+            <div class="pt-3">
+                Было собрано семантическое ядро на 50 000 ключевых запросов. Рекламный бюджет 150 000 рублей. Заявок 150 заявок в месяц. Продаж 2 млн. рублей.
+            </div>
+        </div>
+    </div>
+</div>
+
+@component("components.form-3")
+    @slot("bgimageid")
+        bg-img-form
+    @endslot
+    @slot("image")
+        /images/birja-specialistov-konteksta/birja-specialistov-konteksta-2.jpg
+    @endslot
+    @slot("title")
+        Опишите что требуется сделать и с вами свяжутся несколько специалистов из которых вы выберите лучшего
+    @endslot
+    @slot("source")
+        pilka-scholl
+    @endslot
+    @slot("explanations")
+        Email и телефон для связи. В комментариях опишите цель и задачи, которые требуется решить.
+    @endslot
+    @slot("calltoaction")
+        Отправить
+    @endslot
+    @slot("jurist")
+        © Genlid 2017 Все права защищены
+        <br>
+        <a href="/files/politika-confidencialnosti.pdf" target="_blank">Политика конфиденциальности</a>
+        <br>
+        Санкт-Петербург, проспект Кима 6 <br>
+        ИНН 7726338554 КПП 772601001 ОГРН 1157746406850
+    @endslot
+    @slot("contacts")
+        Телефон <a href="callto:8 929 116 85 65">8 929 116 85 65</a><br>
+        Email <a href="mailto:nitrolovsky@mail.ru">nitrolovsky@mail.ru</a><br>
+        Дата обновления сайта 20.02.2017<br>
+        Сайт разработал <a href="https://vk.com/id327409638">Pavel Nitrolovsky</a>
+    @endslot
+@endcomponent
+
+@component("components.js")
+@endcomponent
