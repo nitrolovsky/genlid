837fa8a262be243c5b39b195258a114051c12ab1
nitrolovsky <nitrolovsky@gmail.com>
2017-02-26 23:56:16 +0300

    Added white border

--- resources/views/page/showG.blade.php
+++ resources/views/page/showG.blade.php
@@ -25,6 +25,9 @@ a {
 a:hover {
     color: #546fff !important;
 }
+.borderwhite {
+    border: 1px solid white;
+}
 </style>
 <div class="container-fluid p-0">
     <div class="container-fluid px-0">
@@ -65,7 +68,7 @@ a:hover {
     <div class="container">
         <div class="row">
             <div class="col-xl-8 col-lg-8">
-                <div class="embed-responsive embed-responsive-16by9" id="video">
+                <div class="embed-responsive embed-responsive-16by9 borderwhite" id="video">
                     <iframe class="embed-responsive-item" src="{{ $page->video }}" allowfullscreen></iframe>
                 </div>
             </div>
