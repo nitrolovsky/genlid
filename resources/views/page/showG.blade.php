719c59be8d8cc94eb59ccf55faf889acb62203b4
Pavel L <nitrolovsky@gmail.com>
2017-03-02 16:56:50 +0300

    Added metrics

--- resources/views/page/showG.blade.php
+++ resources/views/page/showG.blade.php
@@ -97,7 +97,7 @@
                         </div>
                         <div class="form-group">
                             <div class="">
-                                <button type="submit" class="btn-circle btn btn-primary btn-block">{{ $page->call_to_action }}</button>
+                                <button type="submit" class="btn-circle btn btn-primary btn-block" onclick="yaCounter42928359.reachGoal('Proposal'); return true;">{{ $page->call_to_action }}</button>
                             </div>
                         </div>
                     </form>
