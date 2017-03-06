3b9a9ec45b8428d773914a94545b991a7d4fd0e2
Pavel L <nitrolovsky@gmail.com>
2017-03-06 16:52:03 +0300

    Changed form

--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -50,22 +50,91 @@
                 </div>
             </div>
         </nav>
-        <div class="container padding-top">
-            <div class="row">
-                <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1 col-md-12 col-sm-12 col-12">
-                    <h1 class="h2 russo pt-5">
-                        Разрабатываем 
-                    </h1>
-                    <ul class="h5 russo pt-3 pb-5">
-                        <li class="py-1">Гарантирую заявки в течении 2 недель с запуска рекламы</li>
-                        <li class="py-1">A/B тестирование 3 вариантов до рентабельной конверсии</li>
-                        <li class="py-1">Хорошо отображаются на мобильных устройствах</li>
-                        <li class="py-1">Уведомления с контактами клиентов на ваш email</li>
-                        <li class="py-1">Настроенная аналитика в Яндекс Метрики и Google Analytics</li>
-                        <li class="py-1">Текст в информационном стиле Максима Ильяхова</li>
-                    </ul>
+
+        <style>
+        .bgimage-first {
+            background: url(/images/lendingi/lendingi-2.jpg) no-repeat center center;
+            background-size: cover;
+            background-color: rgba(0, 0, 0, 0.5);
+            background-blend-mode: overlay;
+            color: white;
+        }
+        </style>
+
+        <div class="container-fluid p-0 bgimage-first pt-5">
+            <div class="container-fluid px-0">
+                <div class="container py-5">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 text-xl-left text-lg-left text-md-left text-sm-center text-center">
+                            <div class="row">
+                                <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-xs-10">
+                                    <span class="russo">genlid.com</span><br>
+                                    Создание продающих сайтов
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 text-xl-right text-lg-right text-md-right text-sm-center text-center">
+                            <div class="hidden-md-up">
+                                <br>
+                            </div>
+                            +7 929 116 85 65<br>
+                            nitrolovsky@gmail.com
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1 col-md-12 col-sm-12 col-12">
+                        <h1 class="h2 russo text-center mb-0">
+                            Разрабатываем эффективные Landing Page, которые превращают посетителей в клиентов с гарантией результата
+                        </h1>
+                        <ul class="h5 russo pt-5">
+                            <li class="py-1">От 15 000 рублей в течении 3 дней</li>
+                            <li class="py-1">A/B тестирование 3 вариантов до рентабельной конверсии</li>
+                            <li class="py-1">Хорошо отображаются на мобильных устройствах</li>
+                            <li class="py-1">Контакты клиентов приходят на email и CRM</li>
+                            <li class="py-1">Настроенная аналитика в Яндекс Метрики и Google Analytics</li>
+                            <li class="py-1">Редактура текста в информационном стиле</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container py-5">
+                <div class="row">
+                    <form class="col-xl-10 offset-xl-1 col-lg-12 col-md-12 col-sm-12 col-12" action="/proposals" method="POST">
+                        <div class="form-inline">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="product_url" value="videoroliki">
+                            <input type="hidden" name="type" value="Order from article">
+                            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                                <div class="form-group {{ !empty($errors->orders->first('email')) ? 'has-danger' : ''}} {{ !empty(old('email')) ? 'has-success' : ''}}">
+                                    <input type="email" class="sw form-control col-12 btn-circle {{ !empty($errors->orders->first('email')) ? 'form-control-danger' : ''}} {{ !empty(old('email')) ? 'form-control-success' : ''}}" id="email" placeholder="Email" name="email" value="{{ old('email') }}">
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                                <div class="form-group {{ !empty($errors->orders->first('phone')) ? 'has-danger' : ''}} {{ !empty(old('phone')) ? 'has-success' : ''}}">
+                                    <input type="phone" class="sw form-control col-12 btn-circle {{ !empty($errors->orders->first('phone')) ? 'form-control-danger' : ''}} {{ !empty(old('phone')) ? 'form-control-success' : ''}}" id="phone" placeholder="Телефон" name="phone" value="{{ old('phone') }}">
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                                <button type="submit" class="sw btn btn-primary btn-block btn-circle" onclick="yaCounter42033909.reachGoal('OrderByPhone'); return true;">
+                                    Отправить
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="container py-5">
+            <h1 class="h2 russo text-center">
+                Посмотрите примеры Landing pages
+            </h1>
+        </div>
+
+        <div class="container">
             <div class="row pb-3">
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
                     <a href="http://genlid.com/pages/25/g" target="_blank">
