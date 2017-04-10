cc048b517611a190443c6759253f58606e5fc473
nitrolovsky <nitrolovsky@gmail.com>
2017-04-10 16:03:42 +0300

    Added kvartinvest

---
+++ resources/views/lp/kvartinvest.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/lp.css">
+        <title>
+            Инвестирование в недвижимость СПб по уму
+        </title>
+        <style>
+            .bg-img {
+                background: url("/files/kvartinvest/bg.jpg") no-repeat top center;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="bg-img">
+            <div class="bg-overlay">
+                <div class="container font">
+                    <div class="row pt-4">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 text-xl-left text-lg-left text-md-left text-sm-center text-center">
+                            <span class="h2 text-uppercase font">
+                                 СПб Инвест
+                            </span><br>
+                            <span class="descriptor">Инвестирование в недвижимость СПб по уму</span>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 text-xl-right text-lg-right text-md-right text-sm-center text-center">
+                            <div class="hidden-md-up">
+                                <br>
+                            </div>
+                            <a href="tel:88126424265" class="a text-info">8 (812) 642 42 65</a><br>
+                            Санкт-Петербург<br> Каменноостровский проспект дом 26-28<br>Дом Бенуа
+                        </div>
+                    </div>
+                    <div class="row pt-5">
+                        <div class="col-xl-10 col-lg-12 col-md-12 col-sm-12 col-12 offset-xl-1">
+                            <h1 class="text-center my-0 weight-700">
+                                21 апреля 2017 в 19:15 - Семинар "Как купить себе квартиру уже в 2017 году" или заработать на недвижимости
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="row pt-5">
+                        <div class="col-xl-10 col-lg-12 col-md-12 col-sm-12 col-12 offset-xl-1">
+                            <div class="my-0 text-center">
+                                <p class="font bullets">
+                                    Разбор конкретных примеров за 2015 и 2016 год с доходностью 58%.<br>
+                                    Анонсы новых объектов и бронирование на старте продаж.<br>
+                                    Как обойти уловки менеджеров отдела продаж застройщика и купить то, что Вам реально нужно.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row pt-5">
+                        <div class="col-xl-8 col-lg-8">
+                            <div class="embed-responsive embed-responsive-16by9 sw" id="video">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/M32KCq5_X6Q?rel=0&showinfo=0" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-6 offset-xl-0 offset-lg-0 offset-md-3">
+                            <div class="hidden-lg-up">
+                                <br>
+                            </div>
+                            <h3 class="px-2 pb-3 text-center weight-700">
+                                Зарегистрируйтесь на семинар
+                            </h3>
+                            <form action="/leads/beautykitchen" method="POST">
+                                {{ csrf_field() }}
+                                <div class="form-group">
+                                    <div class="">
+                                        <input type="text" class="font black sw btn-circle form-control" id="name" placeholder="Имя" name="name">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="">
+                                        <input type="text" class="font black sw btn-circle form-control" id="email" placeholder="Email" name="email" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="">
+                                        <input type="text" class="font black sw btn-circle form-control" id="phone" placeholder="Телефон" name="phone" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="">
+                                        <button type="submit" class="font sw btn-circle btn btn-primary btn-block" role="button">Зарегистрироваться бесплатно</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row pt-5 pb-4">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-xs-6 col-12 text-xl-left text-lg-left text-md-left text-sm-center text-center">
+                             © 2017 СПб Инвест
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-xs-6 col-12 text-xl-right text-lg-right text-md-right text-sm-center text-center">
+                            <div class="hidden-md-up">
+                                <br>
+                            </div>
+                            <a href="tel:88126424265" class="a text-info">8 (812) 642 42 65</a><br>
+                            Санкт-Петербург<br> Каменноостровский проспект дом 26-28<br>Дом Бенуа
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter44019904 = new Ya.Metrika({
+                            id:44019904,
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true,
+                            trackHash:true,
+                            ecommerce:"dataLayer"
+                        });
+                    } catch(e) { }
+                });
+
+                var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks");
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/44019904" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+    </body>
+</html>
